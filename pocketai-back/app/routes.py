bd143ab4a15324d125be8a3b6778374a3d3dcbee
Rodrigo Villar <rodrigovillarvalenca@gmail.com>
2024-10-01 12:37:44 -0300

    instala o cors no app flask, instala axios no react, configura o CORS no arquivo __init__ dentro de utils no flask, cria a rota get dentro do flask

--- pocketai-back/app/routes.py
+++ pocketai-back/app/routes.py
@@ -5,25 +5,26 @@ main_bp = Blueprint('main', __name__)
 
 data_store = {}
 
-@main_bp.route('/items', methods=['POST'])
-def create_item():
-    data = request.get_json()
-    new_id = create_new_item(data_store, data['name'], data['description'])
-    return jsonify({'message': 'Item created successfully', 'id': new_id}), 201
-
-@main_bp.route('/items', methods=['GET'])
+@main_bp.route('/get', methods=['GET'])
 def get_items():
-    return jsonify(get_all_items(data_store))
+    return jsonify({'message': 'deu get'}), 201
 
-@main_bp.route('/items/<int:item_id>', methods=['PUT'])
-def update_item_route(item_id):
-    data = request.get_json()
-    if not update_item(data_store, item_id, data['name'], data['description']):
-        return jsonify({'message': 'Item not found'}), 404
-    return jsonify({'message': 'Item updated successfully'})
+# @main_bp.route('/items', methods=['POST'])
+# def create_item():
+#     data = request.get_json()
+#     new_id = create_new_item(data_store, data['name'], data['description'])
+#     return jsonify({'message': 'Item created successfully', 'id': new_id}), 201
 
-@main_bp.route('/items/<int:item_id>', methods=['DELETE'])
-def delete_item_route(item_id):
-    if not delete_item(data_store, item_id):
-        return jsonify({'message': 'Item not found'}), 404
-    return jsonify({'message': 'Item deleted successfully'})
+
+# @main_bp.route('/items/<int:item_id>', methods=['PUT'])
+# def update_item_route(item_id):
+#     data = request.get_json()
+#     if not update_item(data_store, item_id, data['name'], data['description']):
+#         return jsonify({'message': 'Item not found'}), 404
+#     return jsonify({'message': 'Item updated successfully'})
+
+# @main_bp.route('/items/<int:item_id>', methods=['DELETE'])
+# def delete_item_route(item_id):
+#     if not delete_item(data_store, item_id):
+#         return jsonify({'message': 'Item not found'}), 404
+#     return jsonify({'message': 'Item deleted successfully'})
